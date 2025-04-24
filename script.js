839c124b6bf44b86f3c8997385b6406231d2c2b5
Gustavo Machado <gustavomachadoespindola49@gmail.com>
2025-04-24 19:58:39 -0300

    Primeira versão funcional do projeto DevTasks.
    1. adição de tarefas
    2. remoção de tarefas
    3. marcar como concluída.

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
-//Selecionando elementos do DOM.
+//Selecionando elementos do DOM
 const form = document.getElementById('task-form');
 const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
 
 
-//Função que cria um novo item na lista.
+//Função que cria um novo item na lista
 function addTask(text) {
     const li = document.createElement('li');
     li.classList.add('task-item');
-    li.textContent = text;
 
-    taskList.appendChild(li);
+
+const taskText = document.createElement('span');
+taskText.textContent = text;
+taskText.classList.add('task-text');
+
+const removeButton = document.createElement('button');
+removeButton.textContent = ' ❌ '
+removeButton.classList.add('remove-btn');
+
+
+// Ao clicar no texto, marca/desmarca como tarefa concluída
+taskText.addEventListener('click', function (){
+    taskText.classList.toggle('done');
+});
+
+li.appendChild(taskText);
+li.appendChild(removeButton);
+taskList.appendChild(li);
+
+removeButton.addEventListener('click', function(){
+    li.remove();
+});
+
+
+//Adiciona a tarefa e o botão no li
+
+li.appendChild(taskText);
+li.appendChild(removeButton);
+
+taskList.appendChild(li);
+
+//Adiciona o evento de remover a tarefa ao botão
+
+removeButton.addEventListener('click', function() {
+    li.remove(); //Remove o item da lista
+});
+
 }
 
 
-//Escutando o envio do Formulário.
+
+
+
+
+//Escutando o envio do Formulário
 
 form.addEventListener ('submit', function(event){
-    event.preventDefault(); //Impedindo o recarregamento da Página.
+    event.preventDefault(); //Impedindo o recarregamento da Página
 
 
-    const taskText = taskInput.value.trim(); //Pegando e limpando o valor.
+    const taskText = taskInput.value.trim(); //Pegando e limpando o valor
 
     if (taskText !== '') {
-        addTask(taskText); //Adiciona na Lista.
-        taskInput.value = ''; //Limpa o Input.
+        addTask(taskText); //Adiciona na Lista
+        taskInput.value = ''; //Limpa o Input
     }
 });
 
